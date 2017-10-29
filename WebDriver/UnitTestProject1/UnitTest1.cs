b182110e42a3215c5699c177d1ddc5511c1b08eb
alinakohnovich6697 <lina.kohnovich@gmail.com>
2017-10-30 01:35:19 +0300

    Add files via upload

---
+++ WebDriver/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private static IWebDriver webDriverChrome;
+        [AssemblyInitialize]
+        public static void One(TestContext testContext)
+        {
+            try
+            {
+                webDriverChrome = new ChromeDriver(@"D:\УЧЁБА\4 курс\7 cеместр\ЕПАМ\chrome");
+            }
+            catch (Exception exception)
+            {
+                exception.StackTrace.ToString();
+            }
+
+        }
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            webDriverChrome.Navigate().GoToUrl("http://www.github.com/");
+            webDriverChrome.FindElement(By.XPath("//a[text() = 'Sign in']")).Click();
+            
+            webDriverChrome.FindElement(By.XPath("//input[@id='login_field']")).SendKeys(" testautomationuser");
+            webDriverChrome.FindElement(By.XPath("//input[@id='password']")).SendKeys("Time4Death!");
+
+            System.Threading.Thread.Sleep(10000);
+        }
+        [TestCleanup]
+        public void TearDown()
+        {
+            webDriverChrome.Quit();
+        }
+    }
+}
